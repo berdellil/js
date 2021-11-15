5150c98dd39fbc5b880ed645ba2a87b5abeacd02
berdellil <hind.berdellil@lepoles.org>
2021-11-15 15:50:31 +0100

    js

---
+++ js/02_concatenation.js
@@ -0,0 +1,38 @@
+alert("c'est bon"); //on a teter notre vhemin avec une foncrtion alertt()
+    /*----------------------------------
+    La  CONCATENATION
+    ---------------*/
+  
+var annee = 2017; //variable de type nombre
+
+var futur = 5;
+
+var calcul = annee + futur;
+
+console.log(calcul);
+
+document.write(calcul + "<br>");// afiche le resultat sur la page avec un br
+
+
+
+var mois = "7";
+
+var calcul2 = annee + mois;
+
+console.log(calcul2);
+
+document.write(calcul2 + "<br>");
+
+
+
+var debutPhrase = "Aujourd'hui ";
+
+var nbStragiaires = 11;
+
+var suitePhrase = " Stagiares";
+
+var finPhrase = "sont présent.";
+
+document.write(debutPhrase + nbStragiaires + suitePhrase + " " + finPhrase);
+
+//Le " " est souvent utilisé lors de la récupération de données saisies pas L'utilisateur
